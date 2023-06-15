e153f165f5aa17440ea0766e69e8920b223c67cc
russ4stall <russ@slashpine.tech>
2023-06-15 13:31:02 -0400

    moved js to initial load

--- src/site.js
+++ src/site.js
@@ -14,3 +14,31 @@ class HandlebarsPartial {
 //         evt.detail.requestConfig.elt.reset();
 //     }
 // });
+
+function fetchTrails() {
+    fetch('https://trails.apps.fourscorepicks.com/trails')
+        .then(res => res.json())
+        .then((out) => {
+            var html = `<table class="table">
+                <thead>
+                    <th>Name</th>
+                    <th>Forest</th>
+                    <th>Length</th>
+                    <th></th>
+                </thead>`;
+            
+            for(var i=0; i<out.length; i++) {
+                html = html +
+                `<tr>
+                    <td>${out[i].name}</td>
+                    <td>${out[i].stateForestName}</td>
+                    <td>${out[i].length}</td>
+                    <td>${JSON.stringify(out[i])}</td>
+                </tr>`;
+            }
+
+            html = html + '</table>';
+            document.querySelector('#fill-me-up').innerHTML = html;
+        })
+        .catch(err => console.error(err));
+}
